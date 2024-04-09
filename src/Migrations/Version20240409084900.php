c0beaa5d9d86283e0e65928ef02e2202f1634441
Christoph Reiter <reiter.christoph@gmail.com>
2024-04-09 13:03:24 +0200

    SQL typo

--- src/Migrations/Version20240409084900.php
+++ src/Migrations/Version20240409084900.php
@@ -19,7 +19,7 @@ final class Version20240409084900 extends EntityManagerMigration
     public function up(Schema $schema): void
     {
         $this->addSql('CREATE TABLE authorization_resource_actions (
-            identifier VARCHAR(50) NOT NULL, namespace VARCHAR(64), NOT NULL, resource_identifier VARCHAR(64),
+            identifier VARCHAR(50) NOT NULL, namespace VARCHAR(64) NOT NULL, resource_identifier VARCHAR(64),
             action VARCHAR(64), PRIMARY KEY(identifier)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
