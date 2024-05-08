2653e80fc39e7a2bafda4d099eccbd7254f80155
Tobias Gross-Vogt <tobias.gross-vogt@tugraz.at>
2024-05-08 09:58:27 +0200

    add TestResourceActionGrantServiceFactory for other bundles to get a ResourceActionGrantService for unit testing

---
+++ src/TestUtils/TestResourceActionGrantServiceFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dbp\Relay\AuthorizationBundle\TestUtils;
+
+use Dbp\Relay\AuthorizationBundle\API\ResourceActionGrantService;
+use Dbp\Relay\AuthorizationBundle\Authorization\AuthorizationService;
+use Dbp\Relay\AuthorizationBundle\Service\GroupService;
+use Dbp\Relay\AuthorizationBundle\Service\InternalResourceActionGrantService;
+use Dbp\Relay\CoreBundle\TestUtils\TestAuthorizationService;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+class TestResourceActionGrantServiceFactory
+{
+    public static function createTestResourceActionGrantService(KernelInterface $kernel,
+        string $currentUserIdentifier = TestAuthorizationService::TEST_USER_IDENTIFIER,
+        array $currentUserAttributes = []): ResourceActionGrantService
+    {
+        $testEntityManager = new TestEntityManager($kernel);
+        $internalResourceActionGrantService = new InternalResourceActionGrantService(
+            $testEntityManager->getEntityManager());
+        $authorizationService = new AuthorizationService(
+            $internalResourceActionGrantService, new GroupService($testEntityManager->getEntityManager()));
+        TestAuthorizationService::setUp($authorizationService, $currentUserIdentifier, $currentUserAttributes);
+        $authorizationService->setConfig(self::getTestConfig());
+
+        return new ResourceActionGrantService($authorizationService);
+    }
+
+    private static function getTestConfig(): array
+    {
+        return [
+            'create_groups_policy' => 'user.get("MAY_CREATE_GROUPS")',
+        ];
+    }
+}
