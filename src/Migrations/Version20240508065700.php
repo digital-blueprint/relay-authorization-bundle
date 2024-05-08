f98fd5b506f43351352d05390747c9ee7a99f11d
Tobias Gross-Vogt <tobias.gross-vogt@tugraz.at>
2024-05-08 09:17:14 +0200

    add migrations; release

---
+++ src/Migrations/Version20240508065700.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dbp\Relay\AuthorizationBundle\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240508065700 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'delete deprecate version of table authorization_resource_actions';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('DROP TABLE authorization_resource_actions');
+    }
+
+    public function down(Schema $schema): void
+    {
+    }
+}
