60510298369fe6acd5f564a1fdef2269685a67fe
Tobias Gross-Vogt <tobias.gross-vogt@tugraz.at>
2024-06-18 14:53:54 +0200

    add migration to remove the unique constraint from authorization_group_members.child_group_identifier

---
+++ src/Migrations/Version20240618123132.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dbp\Relay\AuthorizationBundle\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240618123132 extends EntityManagerMigration
+{
+    public function getDescription(): string
+    {
+        return 'replace the false authorization_group_members.child_group_identifier unique constraint by a normal index';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE authorization_group_members DROP INDEX UNIQ_D819EFC5E50822CE, ADD INDEX IDX_D819EFC5E50822CE (child_group_identifier)');
+    }
+
+    public function down(Schema $schema): void
+    {
+    }
+}
