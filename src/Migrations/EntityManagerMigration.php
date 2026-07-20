7b664aee5aadf158f8ee577fa1b5601b9db1ad4f
Tobias Gross-Vogt <tobias.gross-vogt@tugraz.at>
2026-07-20 10:03:26 +0200

    allow to specify an identifier when adding roles; add manager role

--- src/Migrations/EntityManagerMigration.php
+++ src/Migrations/EntityManagerMigration.php
@@ -4,14 +4,13 @@ declare(strict_types=1);
 
 namespace Dbp\Relay\AuthorizationBundle\Migrations;
 
+use Dbp\Relay\AuthorizationBundle\DependencyInjection\DbpRelayAuthorizationExtension;
 use Dbp\Relay\CoreBundle\Doctrine\AbstractEntityManagerMigration;
 
 abstract class EntityManagerMigration extends AbstractEntityManagerMigration
 {
-    private const EM_NAME = 'dbp_relay_authorization_bundle';
-
     protected function getEntityManagerId(): string
     {
-        return self::EM_NAME;
+        return DbpRelayAuthorizationExtension::AUTHORIZATION_ENTITY_MANAGER_ID;
     }
 }
